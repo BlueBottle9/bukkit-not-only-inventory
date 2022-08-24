24b46bc57ff4ba18023bc86c2ac79365272cdde0
BlueBottle9 <107334439+BlueBottle9@users.noreply.github.com>
2022-08-24 23:35:04 +0800

    v0.0.1
    Early test

---
+++ src/main/java/bluescreen9/minecraft/bukkit/notonlyinventory/Clickable.java
@@ -0,0 +1,7 @@
+package bluescreen9.minecraft.bukkit.notonlyinventory;
+
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+public interface Clickable {
+				public void onClick(InventoryClickEvent event);
+}
