fd2c6e2904793f3516361930b13e84b2cbc82ee3
BlueBottle9 <107334439+BlueBottle9@users.noreply.github.com>
2022-08-26 12:08:07 +0800

    Delete src/main/java/bluescreen9/minecraft/bukkit/notonlyinventory directory

--- src/main/java/bluescreen9/minecraft/bukkit/notonlyinventory/Clickable.java
+++
@@ -1,7 +0,0 @@
-package bluescreen9.minecraft.bukkit.notonlyinventory;
-
-import org.bukkit.event.inventory.InventoryClickEvent;
-
-public interface Clickable {
-				public void onClick(InventoryClickEvent event);
-}
